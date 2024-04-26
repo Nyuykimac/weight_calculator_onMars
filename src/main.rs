ad0d3a8ff4bd97e9e7ec739648bd40a7ff660e0f
Nyuykimac <macnyuyki@gmail.com>
2024-04-26 20:18:52 +0100

    implemented Mars weight calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the weight here: (kg)");
+    let mut input = String::new();
+
+    let _ = io::stdin().read_line(&mut input).unwrap();
+
+    let value = input.trim().parse().unwrap();
+    let weight = weight_calculator(value);
+    println!("The weight in Mars is: {}", weight);
+
+    println!("Operation completed Successfully")
+}
+
+fn weight_calculator(weight: f32) -> f32 {
+    let weight_on_mars = (weight / 9.81) * 3.711;
+    return weight_on_mars;
 }
